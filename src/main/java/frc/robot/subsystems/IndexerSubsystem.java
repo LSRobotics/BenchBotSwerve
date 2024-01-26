e923611bfb804016369435bc6721d0340b702223
StauferM <123831985+StauferM@users.noreply.github.com>
2024-01-26 15:23:42 -0500

    Fixed IndexerSubsystem again

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,15 +1,17 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IndexerConstants;
 
 public class IndexerSubsystem extends SubsystemBase {
-    public TalonSRX indexMotor;
+    public WPI_TalonSRX indexMotor;
     public DigitalInput indexBeamBreak;
 
     public IndexerSubsystem() {
-        indexMotor = new TalonSRX(IndexerConstants.indexMotorID);
+        indexMotor = new WPI_TalonSRX(IndexerConstants.indexMotorID);
 
         indexBeamBreak = new DigitalInput(IndexerConstants.indexBeamBreakChannel);
     }
